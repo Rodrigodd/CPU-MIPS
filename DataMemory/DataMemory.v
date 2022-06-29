2456bc4450e5a0dd3dd7f3dc940ad432019420e0
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-06-29 20:22:39 -0300

    implement DataMemory

--- DataMemory/DataMemory.v
+++ DataMemory/DataMemory.v
@@ -1,10 +1,41 @@
-module DataMemory(
+module DataMemory #(
+	parameter INITIAL = "data.txt",
+	parameter DATA_WIDTH = 32,
+	parameter ADDR_WIDTH = 10
+) (
 	input clk, rst,
-	input [31:0] addr,
-	input [31:0] data_in,
+	input [ADDR_WIDTH-1:0] addr,
+	input [DATA_WIDTH-1:0] data_in,
 	input wr_rd, // 0 is write, 1 is read
-	output [31:0] data_out
+	output [DATA_WIDTH-1:0] data_out
 );
+
+// Declare the RAM variable
+reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+
+// Variable to hold the registered read address
+reg [ADDR_WIDTH-1:0] addr_reg;
+
+integer i;
+
+initial begin for (i = 0; i < 2**ADDR_WIDTH - 1; i = i + 1) begin
+		ram[i] = 0;
+	end
+	$readmemh(INITIAL, ram, 0);
+end
+
+always @ (posedge clk) begin
+	// Write
+	if (wr_rd == 0)
+		ram[addr] <= data_in;
+
+	addr_reg <= addr;
+end
+
+// Continuous assignment implies read returns NEW data.
+// This is the natural behavior of the TriMatrix memory
+// blocks in Single Port mode.  
+assign data_out = ram[addr_reg];
 
 endmodule
 
