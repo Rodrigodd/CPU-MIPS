a41d5423af5e23dd010c528509b48d8f260b29e7
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-06-29 16:25:40 -0300

    implemented InstructionMemory

--- InstructionMemory/InstructionMemory.v
+++ InstructionMemory/InstructionMemory.v
@@ -1,7 +1,31 @@
-module InstructionMemory(
+module InstructionMemory #(
+	parameter INITIAL = "code.txt",
+	parameter DATA_WIDTH = 32,
+	parameter ADDR_WIDTH = 10
+) (
 	input clk, rst,
-	input [31:0] addr,
-	output [31:0] data
+	input [ADDR_WIDTH-1:0] addr,
+	output reg [DATA_WIDTH-1:0] data
 );
+
+
+// Declare the ROM variable
+reg [DATA_WIDTH-1:0] rom[2**ADDR_WIDTH-1:0];
+
+integer i;
+
+initial
+begin
+	for (i = 0; i < 2**ADDR_WIDTH - 1; i = i + 1) begin
+		rom[i] = 0;
+	end
+	$readmemh(INITIAL, rom, 0);
+end
+
+always @ (posedge clk)
+begin
+	data <= rst ? 0 : rom[addr];
+end
+
 endmodule
 
