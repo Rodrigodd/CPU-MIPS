862238e380c4e45e6b6beaa9e18d263dc95b2d0d
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-06-29 16:48:50 -0300

    implement RegisterFile

--- RegisterFile/RegisterFile.v
+++ RegisterFile/RegisterFile.v
@@ -4,8 +4,25 @@ module RegisterFile(
 	input [4:0] write_back_reg, 
 	input [31:0] write_back,
 	input [4:0] a_reg, b_reg,
-	output [31:0] a_ex, b_ex
+	output reg [31:0] a, b
 );
+
+reg [31:0] registers [31:0];
+
+integer i;
+
+always @(posedge clk or posedge rst) begin
+	if (rst) begin
+		// clear all registers
+		for (i = 0; i < 32; i = i + 1) begin
+			registers[i] = 0;
+		end
+	end else begin
+		if (write_back_en) registers[write_back_reg] <= write_back;
+		a <= registers[a_reg];
+		b <= registers[b_reg];
+	end
+end
 
 endmodule
 
