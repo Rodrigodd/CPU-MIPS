559bca9deb3c5e90273721455e00a661f7f8355a
Jessé Júnior <jessejuniormaciel@hotmail.com>
2022-07-13 09:55:37 -0300

    Mux_TB adicionado

--- MUX/MUX_TB.v
+++ MUX/MUX_TB.v
@@ -1,25 +1,44 @@
 `timescale 1ns/1ps
 module MUX_TB();
 
-reg clk;
-reg rst;
+reg [31:0] a, b;
+reg sel;
+wire [31:0] q; 
 
 MUX DUT(
-	clk, rst
+	.a(a),
+	.b(b),
+	.sel(sel),
+	.q(q)
 );
 
 initial begin
-	clk = 0;
-	rst = 1;
-	#20
-	rst = 0;
+	sel <= 0;
+	a <= 32'd1;
+	b <= 32'd2;
+		 
+		 
+	#20 sel <= 1;
+		 
+	#20 a <= 32'd3; b <= 32'd4; sel <= 0;
+		 
+	#20 sel <= 1;
+	
+	#20 a <= 32'd5; b <= 32'd6; sel <= 0;
+		 
+	#20 sel <= 1;
+	
+	#20 a <= 32'd7; b <= 32'd8; sel <= 0;
+		 
+	#20 sel <= 1;
+	
+	#20 a <= 32'd9; b <= 32'd10; sel <= 0;
+		 
+	#20 sel <= 1;
 
 	#200
-	$stop();
+$stop();
 end
-
-// f = 50 MHz => Tc = 20
-always #10 clk = ~clk;
 
 endmodule
 
