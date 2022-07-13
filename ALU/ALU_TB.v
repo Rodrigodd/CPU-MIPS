b4d286303db817ae7e45b7b3b39d71d877316594
Jessé Júnior <jessejuniormaciel@hotmail.com>
2022-07-13 11:30:53 -0300

    ALU_TB adicionado

--- ALU/ALU_TB.v
+++ ALU/ALU_TB.v
@@ -1,25 +1,50 @@
 `timescale 1ns/1ps
 module ALU_TB();
 
-reg clk;
-reg rst;
+reg [31:0] a, c;
+reg [1:0] op;
+wire  [31:0] out;
 
 ALU DUT(
-	clk, rst
+	.a(a), 
+	.c(c), 
+	.op(op), 
+	.out(out)
 );
 
 initial begin
-	clk = 0;
-	rst = 1;
-	#20
-	rst = 0;
-
+	a <= 32'h0000000A;
+	c <= 32'h00000001;
+	op <= 0;
+	
+	#20 op <= 1;
+	#20 op <= 2;
+	#20 op <= 3;
+	
+	#20 
+	a <= 32'h0000000F;
+	c <= 32'h00000003;
+	op <= 0;
+	
+	#20 op <= 1;
+	#20 op <= 2;
+	#20 op <= 3;
+	
+	// Testando Overflow
+	#20 
+	a <= 32'hFFFFFFFF;
+	c <= 32'h00000001;
+	op <= 0;
+	
+	// Testando Underflow
+	#20 
+	a <= 32'h00000000;
+	c <= 32'h00000001;
+	op <= 1;
+	
 	#200
 	$stop();
 end
-
-// f = 50 MHz => Tc = 20
-always #10 clk = ~clk;
 
 endmodule
 
