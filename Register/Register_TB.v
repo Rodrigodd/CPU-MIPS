c252e3a5fbe22babeb8f9fcc191e3fc3f1be2809
João Prado <59054486+WinterDP@users.noreply.github.com>
2022-07-13 09:55:12 -0300

    Add instruction Memory TB

--- Register/Register_TB.v
+++ Register/Register_TB.v
@@ -1,18 +1,27 @@
 `timescale 1ns/1ps
 module Register_TB();
 
+parameter WIDTH = 32;
+
 reg clk;
 reg rst;
+reg [WIDTH - 1:0] d;
+wire [WIDTH - 1:0] q;
 
 Register DUT(
-	clk, rst
+	.clk(clk), .rst(rst), .d(d), .q(q)
 );
 
 initial begin
-	clk = 0;
+
 	rst = 1;
-	#20
+	#50
+	d = 1;
+	
+	#50
 	rst = 0;
+	#50
+	d = 0;
 
 	#200
 	$stop();
