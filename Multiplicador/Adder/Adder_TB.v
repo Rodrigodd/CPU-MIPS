dbd9978a2161524178359c78e313dfa1f95d4146
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-07-14 08:00:24 -0300

    fix Adder_TB.v

--- Multiplicador/Adder/Adder_TB.v
+++ Multiplicador/Adder/Adder_TB.v
@@ -1,7 +1,8 @@
+`timescale 1ns/1ps
 module Adder_TB();
 
-wire [4:0] Soma;
-reg [3:0] OperandoA,OperandoB;
+wire [16:0] Soma;
+reg [15:0] OperandoA,OperandoB;
 integer i,j;
 
 Adder DUT(
@@ -13,15 +14,19 @@ Adder DUT(
 initial begin
 	for(i = 0; i<16;i=i+1)
 	begin
-		#10
+		#20
 		OperandoA = i;
 		for(j = 0; j<16;j=j+1)
 		begin
 			OperandoB = j;
-			#10
+			#20
 			$display("OperandoA=%d OperandoB=%d Soma=%d",OperandoA,OperandoB,Soma);
 		end	
 	end	
+	OperandoA = 16'hffff;
+	OperandoB = 16'hffff;
+	#20
+	$display("OperandoA=%x OperandoB=%x Soma=%x",OperandoA,OperandoB,Soma);
 
 end
 	
