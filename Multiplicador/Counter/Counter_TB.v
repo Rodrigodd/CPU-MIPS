4b46a4de6d39b9401fc16cc32d662519c5daf77d
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-07-14 08:22:36 -0300

    fix Counter_TB.v

--- Multiplicador/Counter/Counter_TB.v
+++ Multiplicador/Counter/Counter_TB.v
@@ -1,38 +1,44 @@
 `timescale 1ns/100ps
 module Counter_TB();
-	reg Load, Clk;
-	wire K;
-	
-	reg [3:0]counter;
-	
-	Counter DUT (
-		.Load(Load),
-		.Clk(Clk),
-		.K(K)
-	);
-					 
-					 
-	initial begin	
-		$init_signal_spy("/DUT/counter", "counter", 1);
-	
-		Clk = 0;
 
-		#10
-		Load = 1;
-		#40
-		Load = 0;
-		#180
-		Load = 1;
-		#220
-		Load = 0;
-			  
-		#500
-		$stop;
-			
-	end		
-	always begin
-		#10 Clk = ~Clk;
-	end
+wire K;
+reg Load, Clk, Reset;
+
+reg [3:0]counter;
+
+Counter DUT (
+	.K(K),
+	.Load(Load),
+	.Clk(Clk),
+	.Reset(Reset)
+);
+				 
+				 
+initial begin	
+	$init_signal_spy("/DUT/counter", "counter", 1);
+
+	Clk = 0;
+	Reset = 1;
+
+	#20
+	Reset = 0;
+
+	#10
+	Load = 1;
+	#40
+	Load = 0;
+	#(20*40)
+	Load = 1;
+	#40
+	Load = 0;
+		  
+	#(20*33)
+	$stop;
+		
+end		
+always begin
+	#10 Clk = ~Clk;
+end
 
 
 endmodule 
